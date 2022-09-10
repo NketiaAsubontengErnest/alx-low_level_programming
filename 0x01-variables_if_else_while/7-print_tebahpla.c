bc07f3576df15f6e89b5b71f507c35e7fefbe401
NketiaAsubontengErnest <nketiaernest18@gmail.com>
2022-09-09 17:03:43 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints reversed alphabet.
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
