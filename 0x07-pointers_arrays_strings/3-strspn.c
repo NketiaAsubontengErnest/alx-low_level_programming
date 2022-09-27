f3e602a2cd62b6810343b642bfdf0b907f0c8f25
NketiaAsubontengErnest <nketiaernest18@gmail.com>
2022-09-26 20:09:35 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int x, y = 0, z = 0, k = 0;
+
 	for (x = 0; *(s + x) != 0; x++)
 	{
 		k = 0;
+		
 		for (y = 0;  *(accept + y) != 0; y++)
 		{
 			if (*(s + x) == *(accept + y))
@@ -22,11 +24,6 @@ unsigned int _strspn(char *s, char *accept)
 				k = 1;
 			}
 		}
-		if (k == 0)
-		{
-			return (z);
-		}
 	}
-	return (0);
+	return (z);
 }
-
