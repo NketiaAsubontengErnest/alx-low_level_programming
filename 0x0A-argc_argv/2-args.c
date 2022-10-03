3ff164011e1a024b3fb1d6a481295d4635b5e669
NketiaAsubontengErnest <nketiaernest18@gmail.com>
2022-10-03 14:01:57 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - Entry point
-*
-* @argc: number of arguments
-* @argv: vector of strings of arguments
-*
-* Return: Always 0 (Success)
-*
-*/
-int main(int argc, char *argv[]){
-	int count = 0;
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: vector of strings of arguments
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argc;
+	(void)argv;
 
-	if (argc > 0){
-		while (count < argc){
-			printf("%s\n", argv[count]);
-			count++;
-		}
-	}
+	for (i = 0; i < argc; i++)
+		printf("s\n", argv[i]);
+
 	return (0);
 }
