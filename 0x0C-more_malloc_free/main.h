9b6d4608e6834c6893b915c7d62d3745d74d635b
NketiaAsubontengErnest <nketiaernest18@gmail.com>
2022-10-06 16:01:42 -0700

    Task 3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,10 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_h
+#define MAIN_H
 
+#include <stdio.h>
+#include <stdilib.h>
+#include <limits.h>
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
