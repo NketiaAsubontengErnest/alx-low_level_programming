fe8599e533714c3da0ad4a8405d233a2a738e645
NketiaAsubontengErnest <nketiaernest18@gmail.com>
2022-10-11 16:09:03 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Main function
- *
- * Return: End program
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
+
 int main(void)
 {
-	printf("%s\n",__FILE__);
-
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
-
